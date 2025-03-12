90816b71699a675c775e7f07e7dac7eb06dceb0e
mxcoogi <mxcoogi@gmail.com>
2025-03-12 11:31:24 +0900

    docs : Add java doc lv3

--- src/main/java/org/example/Kiosk.java
+++ src/main/java/org/example/Kiosk.java
@@ -4,15 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author mxcoogi
+ * @version lv3
+ * 키오스크 클래스로 사용자의 입력을 처리하고<br>
+ * List에 MenuItem을 저장하고 있습니다.
+ */
 public class Kiosk {
     private List<MenuItem> menuItems;
     private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * 생성자<br>
+     * 생성시 menuItem ArrayList 할당<br>
+     * init 함수 실행
+     */
     public Kiosk() {
         menuItems = new ArrayList<>();
         init();
     }
 
+    /**
+     * 초기 세팅 키오스크 List에 menuItem 넣어줌
+     */
     private void init() {
         menuItems.add(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
@@ -20,6 +34,9 @@ public class Kiosk {
         menuItems.add(new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
     }
 
+    /**
+     * 키오스크 프로그램을 시작하는 메서드
+     */
     public void start() {
         while (true) {
             showMenuItems();
@@ -27,6 +44,9 @@ public class Kiosk {
         }
     }
 
+    /**
+     * 키오스크의 전체 메뉴를 보여주는 메서드
+     */
     private void showMenuItems() {
         System.out.println("[ SHAKESHACK MENU ]");
         for (int i = 0; i < menuItems.size(); i++) {
@@ -36,6 +56,12 @@ public class Kiosk {
         System.out.println("0. 종료");
     }
 
+    /**
+     * 키오스크 메뉴를 선택하면 정보를  출력해주는 메소드
+     * @throws RuntimeException 0입력시 종료시킴
+     * @throws NumberFormatException 잘못된 숫자 입력
+     * @throws IndexOutOfBoundsException 범위를 넘어가는 숫자 입력
+     */
     private void showMenuItemDescription() {
         String input = scanner.nextLine();
         if (input.equals("0")) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author : mxcoogi
+ * @version : lv3
+ *
+ */
 public class Main {
 
+    /**
+     *
+     * @param args
+     * 키오스크 객체를 생성
+     * start함수로 키오스크 실행
+     */
     public static void main(String[] args){
 
-        /**
-         * @author : mxcoogi
-         * @version : lv2
-         *
-         */
         Kiosk kiosk = new Kiosk();
         kiosk.start();
 
